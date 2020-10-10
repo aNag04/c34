e7a83bf2f1ab4a5b0e494d89b5f95e618ddf2c13
Koushik Kumar Nag <knag@unicef.org>
2020-10-10 12:59:28 +0530

    C 34

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
 var ball;
+var database,position;
 
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var column = database.ref("Ball/Position");
+    column.on("value",readPosition,showError);
+
 }
 
+function readPosition(data){
+    position = data.val();
+    console.log(position.x)
+    ball.x = position.x;
+    ball.y = position.y;
+}
+function showError(){
+    console.log("Error in reading the database")
+}
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("Ball/Position").set({
+        x: position.x + x,
+        y: position.y + y
+    })
 }
